c0814fafd5452440b0a638ee9c5a520aa5d990ef
germanmalykh <malykh.gn@esphere.ru>
2025-07-31 02:12:10 +0300

    update Mascot.jsx

--- src/components/Mascot.jsx
+++ src/components/Mascot.jsx
@@ -5,9 +5,11 @@ export default function Mascot({ selected }) {
     ? "Отличный выбор! Ты управляешь рисками как профи ✅"
     : "Хм, подумай ещё раз — лучше фиксировать риски заранее ⚠️"
 
+  const base = import.meta.env.BASE_URL
+
   return (
     <div style={{ marginTop: 30, textAlign: 'center' }}>
-      <img src="/meerkat_standart.svg" alt="Маскот-сурикат" style={{ height: 200 }} />
+      <img src={`${base}meerkat_standart.svg`} alt="Маскот-сурикат" style={{ height: 200 }} />
       <p style={{ fontSize: 18, marginTop: 10 }}>{message}</p>
     </div>
   )
